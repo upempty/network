bf2454baff5a3fc8cbe1b319828b0244aa6a663d
upempty <52414719+upempty@users.noreply.github.com>
2019-12-28 19:10:45 +0800

    Create ds.py

---
+++ ds/structures/src/ds.py
@@ -0,0 +1,13 @@
+class Graph:
+  class Vertex:
+      def __init__(self, key):
+          self.id = key
+          self.connect_to = {}
+          #{2:88, 3:99}
+  def __init__(self):
+      self.num_vertex = 0
+      self.verlist = {}
+      #{key1: {2:88, 3:99}}
+      #{key2: {2:89, 4:77}}
+      
+      
